cd568187e04f1d04030c534141704c5770c081c8
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-12 17:44:03 -0300

    if e else

--- ativ1/if_else.js
+++ ativ1/if_else.js
@@ -8,3 +8,35 @@ function PassouDeAno(nota){
 
 PassouDeAno(8)
 PassouDeAno(6)
+
+function PassouDeAno(nota){
+    if(nota > 7)
+        console.log(nota + " Passou de ano") //quando tem apenas uma sentença pode ser removida as chaves
+    
+}
+
+PassouDeAno(8)
+PassouDeAno(6)
+
+function PassouDeAno(nota){
+    if(nota > 7); { //utilizando o ponto e vírgula, a sentença que vem após ela não é considerada da condição if
+        console.log(nota + " Passou de ano")
+    }
+    
+}
+
+PassouDeAno(6)
+
+//IF/ELSE (Quando você quer fazer uma tomada de decisão quando retornar falso e true
+
+function PassouDeAno(nota){
+    if(nota > 7){
+        console.log(nota + " Passou de ano")
+    }else{
+        console.log(nota + "Não passou de nao")
+    }
+    
+}
+
+PassouDeAno(6)
+PassouDeAno(8)
